3884b8809619909ebf24bc1509add988743f6ff1
Brayan Luna <blun4n@gmail.com>
2021-03-11 11:15:57 -0600

    Close database connection on destruct

--- TDD2/src/SQLite3Logger.php
+++ TDD2/src/SQLite3Logger.php
@@ -6,9 +6,20 @@ require 'LogInterface.php';
 
 class SQLite3Logger implements Log {
 
-	public function __construct(String $logDataBase) {
-		$bd = new \SQLite3($logDataBase);
-		$bd->close();
+	private $dataBase;
+
+	public function __construct($logDataBase) {
+		$this->dataBase = new \SQLite3($logDataBase);
+	}
+
+	public function __destruct() {
+		$this->dataBase->close();
+	}
+
+	// Only for testing
+	private function replaceDataBase(\SQLite3 $newDataBase) {
+		$this->dataBase->close();
+		$this->dataBase = $newDataBase;
 	}
 	
 	public function writeLog($message): void {
--- TDD2/tests/SQLite3LoggerTest.php
+++ TDD2/tests/SQLite3LoggerTest.php
@@ -14,9 +14,25 @@ class SQLite3LoggerTest extends \PHPUnit\Framework\TestCase {
 		}
 	}
 
+	protected function tearDown(): void {
+		$this->setUp();
+	}
+
 	public function testCreateNewDatabase() {
 		$logger = new SQLite3Logger($this->logDataBase);
 	
 		$this->assertFileExists($this->logDataBase);
+	}
+
+	public function testCloseDatabaseConnectionOnDestruct() {
+		$class = new \ReflectionClass('TDD2\SQLite3Logger');
+		$method = $class->getMethod("replaceDataBase");
+		$method->setAccessible(true);
+		$logger = new SQLite3Logger($this->logDataBase);
+		$mockDataBase = $this->createMock(\SQLite3::class);
+		$method->invokeArgs($logger, array($mockDataBase));
+
+		$mockDataBase->expects($this->once())
+			->method('close');
 	}
 }
