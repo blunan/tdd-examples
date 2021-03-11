b2c67aa6ac8a073788c1f7ea87ac5891a4adc7b2
Brayan Luna <blun4n@gmail.com>
2021-03-10 20:11:27 -0600

    Start from scratch

--- TDD2/TxtLogger.php
+++ TDD2/TxtLogger.php
@@ -4,19 +4,4 @@ namespace TDD2;
 
 class TxtLogger {
 
-	private $file;
-
-	public function __construct($file) {
-		$this->file = $file;
-		fclose(fopen($file, "w"));
-	}
-
-	public function writeLine($message, $tag = '') {
-		$log = fopen($this->file, "a");
-		if(!empty($tag)) {
-			$tag .= ": ";
-		}
-		fwrite($log, $tag . $message . "\n");
-		fclose($log);
-	}
 }
--- TDD2/TxtLoggerTest.php
+++ TDD2/TxtLoggerTest.php
@@ -6,36 +6,4 @@ require 'TxtLogger.php';
 
 class TxtLoggerTest extends \PHPUnit\Framework\TestCase {
 
-	private $logFile = "test.log";
-
-	protected function setUp(): void {
-		if (file_exists($this->logFile)) {
-			unlink($this->logFile);
-		}
-	}
-
-	public function testInitializeTestLog() {
-		$logger = new TxtLogger($this->logFile);
-
-		$this->assertFileExists($this->logFile);
-	}
-
-	public function testWriteOneMessage() {
-		$logger = new TxtLogger($this->logFile);
-		$message = "Mensaje de prueba";
-
-		$logger->writeLine($message);
-
-		$this->assertStringEqualsFile($this->logFile, $message . "\n");
-	}
-
-	public function testWriteMessageWithTag() {
-		$logger = new TxtLogger($this->logFile);
-		$message = "Mensaje de prueba";
-		$tag = "TAG";
-
-		$logger->writeLine($message, $tag);
-
-		$this->assertStringEqualsFile($this->logFile, $tag . ": " . $message . "\n");
-	}
 }
