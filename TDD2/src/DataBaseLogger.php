02c6c6ad863a8ca92ae140c6eef82c73b1ddd2cb
Brayan Luna <blun4n@gmail.com>
2021-03-11 14:09:07 -0600

    Create table on construct

--- TDD2/src/DataBaseLogger.php
+++ TDD2/src/DataBaseLogger.php
@@ -5,6 +5,10 @@ namespace TDD2;
 require 'LogInterface.php';
 
 class DataBaseLogger implements Log {
+
+	public function __construct(\PDO $database) {
+		$database->exec('CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY, tag TEXT, log_message TEXT NOT NULL);');
+	}
 	
 	public function writeLog($message): void {
 	}
--- TDD2/tests/DataBaseLoggerTest.php
+++ TDD2/tests/DataBaseLoggerTest.php
@@ -8,15 +8,10 @@ class DataBaseLoggerTest extends \PHPUnit\Framework\TestCase {
 
 	public function testCreateTableOnConstruct() {
 		$mockDataBase = $this->createMock(\PDO::class);
-		$logger = new DataBaseLogger($mockDataBase);
-
 		$mockDataBase->expects($this->once())
 			->method('exec')
-			->with('CREATE TABLE IF NOT EXISTS logs (
-				id INTEGER PRIMARY KEY,
-				tag TEXT,
-				log_message TEXT NOT NULL
-			);');
-	}
+			->with('CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY, tag TEXT, log_message TEXT NOT NULL);');
 
+		$logger = new DataBaseLogger($mockDataBase);
+	}
 }
